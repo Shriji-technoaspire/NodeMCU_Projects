3a1658ab03d1cacdfaa6869b79a76521bcff660e
Shriji-technoaspire <87520598+Shriji-technoaspire@users.noreply.github.com>
2022-07-29 15:01:52 +0530

    Add files via upload

---
+++ 01 LED Pattern/Left_to_Right/Left_to_Right.ino
@@ -0,0 +1,33 @@
+#define LED1 
+#define LED2 
+#define LED3 
+#define LED4 
+#define LED5 
+#define LED6 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+}

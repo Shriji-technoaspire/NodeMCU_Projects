6bf904596693acfa89559e367fbf158d5bda677e
Shriji-technoaspire <87520598+Shriji-technoaspire@users.noreply.github.com>
2022-08-01 17:22:49 +0530

    Update Right to Left.ino

--- 01 LED Pattern/Right to Left.ino
+++ 01 LED Pattern/Right to Left.ino
@@ -1,1 +1,27 @@
+unsigned int LED[6] = {D0, D1, D3, D5, D7, D8}; 
 
+int i = 0, j=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  for(i=0; i<6; i++)
+  {
+    pinMode(LED[i], OUTPUT);
+    digitalWrite(LED[i], LOW);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(i=6; i>=0; i--)
+  {
+      for(j=6; j>=0; j--)
+      {
+        if(i==j)
+        digitalWrite(LED[j], HIGH);
+        else
+        digitalWrite(LED[j], LOW);
+      }
+     delay(1000);   
+  }   
+}

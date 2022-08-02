35640db4fdcf62543b66b4de0e94a3ff5a53ed89
Shriji-technoaspire <87520598+Shriji-technoaspire@users.noreply.github.com>
2022-08-02 18:25:57 +0530

    Update 06_Object_Distance_measurement.ino

--- 06_Object_Distance_measurement/06_Object_Distance_measurement.ino
+++ 06_Object_Distance_measurement/06_Object_Distance_measurement.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#define echoPin D2
+#define trigPin D1
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+long duration;
+float distance;
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+ }
+void loop(){
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  duration=pulseIn(echoPin,HIGH);
+  distance=(duration*0.034/2);
+  Serial.print("Distance : ");
+  Serial.print(distance);
+  Serial.println(" cm ");
+  delay(1000);
+               
 }

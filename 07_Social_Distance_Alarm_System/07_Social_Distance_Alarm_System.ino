1643e73cee4487eb33fb7766d929a5090e342f9a
Shriji-technoaspire <87520598+Shriji-technoaspire@users.noreply.github.com>
2022-08-02 18:35:10 +0530

    Update 07_Social_Distance_Alarm_System.ino

--- 07_Social_Distance_Alarm_System/07_Social_Distance_Alarm_System.ino
+++ 07_Social_Distance_Alarm_System/07_Social_Distance_Alarm_System.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
-
+int trigPin = D1;
+int echoPin = D2;
+int Buzzer = D6;
+ 
+ 
+void setup() 
+{
+  Serial.begin(9600); 
+  pinMode(Buzzer, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  delay(10);
 
+  Serial.print(distance);
+  Serial.println("CM");
+  
+  if(distance <= 30) 
+    digitalWrite(Buzzer, HIGH);
+  else
+    digitalWrite(Buzzer, LOW);
 }
